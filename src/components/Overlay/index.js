11c21f53f675e406301eb7b14f9a431666bc6af1
Sharavanth <sharavanth.r@gmail.com>
2016-10-23 19:16:19 +0530

    creates an Overlay React Wrapper in component  - no onSelfClick method and listner; this should be handled in the parent Component

---
+++ src/components/Overlay/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+
+export default class Overlay extends React.Component {
+  constructor() {
+    super();
+    this._renderLayer = this._renderLayer.bind(this);
+    this._hideOverlay = this._hideOverlay.bind(this);
+    //this._stopSelfClose = this._stopSelfClose.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("OVERLAY COMPONENT IS MOUNTED");
+    this.overlayElement = document.createElement("div");
+    this.overlayElement.style.zIndex = 1000;
+    document.body.appendChild(this.overlayElement);
+
+    window.addEventListener("click", this._hideOverlay);
+    window.addEventListener("resize", this._renderLayer);
+    //this.overlayElement.addEventListener("click", this._stopSelfClose);
+
+    this._renderLayer();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (nextProps.show !== this.props.show);
+  }
+
+  componentDidUpdate() {
+    this._renderLayer();
+  }
+
+  componentWillUnmount() {
+    console.log("OVERLAY COMPONENT IS GOING TO BE UN-MOUNTED");
+    unmountComponentAtNode(this.overlayElement);
+    document.body.removeChild(this.overlayElement);
+
+    window.removeEventListener("click", this._hideOverlay);
+    window.removeEventListener("resize", this._renderLayer);
+    //this.overlayElement.removeEventListener("click", this._stopSelfClose);
+  }
+
+  _getPos(node) {
+    const pos = node.getBoundingClientRect();
+    return {
+      top    : pos.top + pos.height,
+      left   : pos.left,
+      bottom : pos.bottom,
+      right  : pos.right,
+      height : pos.height,
+      width  : pos.width,
+    };
+  }
+
+  _hideOverlay(event) {
+    if (event.target !== this.overlayElement) {
+      this.props.onHide();
+    }
+  }
+
+  //_stopSelfClose(event) {
+  //  event.stopPropagation();
+  //}
+
+  _renderLayer() {
+    const { show, target } = this.props;
+
+    if (show) {
+      const pos = this._getPos(target);
+      this.overlayElement.style.position = "absolute";
+      this.overlayElement.style.left = `${pos.left}px`;
+      this.overlayElement.style.top = `${pos.top}px`;
+      this.overlayElement.style.width = `${pos.width}px`;
+      render(this.props.children, this.overlayElement);
+    } else {
+      render(<div></div>, this.overlayElement);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+Overlay.propTypes = {
+  show     : React.PropTypes.bool.isRequired,
+  onHide   : React.PropTypes.func.isRequired,
+  target   : React.PropTypes.any.isRequired,
+  children : React.PropTypes.node,
+};

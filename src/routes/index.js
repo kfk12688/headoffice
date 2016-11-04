043faf460c840a4f621cb0393f8d51d7dce21ec7
jasminejanet <jasminegeorge93@gmail.com>
2016-11-04 13:34:22 +0530

    implements bootstrap classes in main App entry point

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,72 +10,96 @@ const isDeveloping = process.env.NODE_ENV !== "production";
 const url = isDeveloping ? "http://localhost:3002" : "http://auth.headofficeapp.in";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { rollUp : true };
-    this.handleRollUpToggle = this.handleRollUpToggle.bind(this);
-    this.logOut = this.logOut.bind(this);
-  }
-
-  componentDidMount() {
-    let claims;
-    try {
-      claims = getUserClaims();
-    } catch (err) {
-      claims = null;
+    constructor(props) {
+        super(props);
+        this.state = {
+            rollUp: true
+        };
+        this.handleRollUpToggle = this.handleRollUpToggle.bind(this);
+        this.logOut = this.logOut.bind(this);
     }
 
-    if (!!claims) {
-      this.props.addCurrentUser(claims);
+    componentDidMount() {
+        let claims;
+        try {
+            claims = getUserClaims();
+        } catch ( err ) {
+            claims = null;
+        }
+
+        if (!!claims) {
+            this.props.addCurrentUser(claims);
+        }
     }
-  }
 
-  handleRollUpToggle() {
-    this.setState({ rollUp : !this.state.rollUp });
-  }
+    handleRollUpToggle() {
+        this.setState({
+            rollUp: !this.state.rollUp
+        });
+    }
 
-  logOut() {
-    clearToken();
-    this.props.removeCurrentUser();
-    window.location = `${url}/logout`;
-  }
+    logOut() {
+        clearToken();
+        this.props.removeCurrentUser();
+        window.location = `${url}/logout`;
+    }
 
-  render() {
-    const { currentUser } = this.props;
+    render() {
+        const {currentUser} = this.props;
 
-    return (
-      <div>
-        <div onClick={this.handleRollUpToggle} className={cx({
-          [styles.rollDownHandle] : !this.state.rollUp,
-          [styles.rollUpHandle]   : this.state.rollUp,
-        })}><span/></div>
-        {this.state.rollUp && <Navigator className={styles.navigatorCtn} user={currentUser} logoutUser={this.logOut}/>}
-        {this.props.children && React.cloneElement(this.props.children, {
-          rollUp : this.state.rollUp,
-        })}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <div className={cx("container-fluid", styles.navigatorCtn)}>
+                    <div
+                        onClick={this.handleRollUpToggle}
+                        className={cx({
+                            [styles.rollDownHandle]: !this.state.rollUp,
+                            [styles.rollUpHandle]: this.state.rollUp,
+                        })}
+                    >
+                        <span/>
+                    </div>
+
+                    {
+                        this.state.rollUp &&
+                        <div className="row">
+                              <div className="col-md-10 offset-md-1">
+                                  <Navigator user={currentUser} logoutUser={this.logOut}/>
+                              </div>
+                        </div>
+                    }
+                </div>
+
+                
+                {
+                    this.props.children && React.cloneElement(this.props.children, {
+                        rollUp: this.state.rollUp,
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 App.propTypes = {
-  children          : React.PropTypes.node.isRequired,
-  currentUser       : React.PropTypes.object.isRequired,
-  removeCurrentUser : React.PropTypes.func.isRequired,
-  addCurrentUser    : React.PropTypes.func.isRequired,
+    children: React.PropTypes.node.isRequired,
+    currentUser: React.PropTypes.object.isRequired,
+    removeCurrentUser: React.PropTypes.func.isRequired,
+    addCurrentUser: React.PropTypes.func.isRequired,
 };
 
 App.contextTypes = {
-  router : React.PropTypes.func.isRequired,
+    router: React.PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   currentUser : state.user.currentUser,
+    currentUser: state.user.currentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
-  removeCurrentUser : () => dispatch(removeCurrentUser()),
-  addCurrentUser    : (claims) => dispatch(addCurrentUser(claims)),
+    removeCurrentUser: () => dispatch(removeCurrentUser()),
+    addCurrentUser: (claims) => dispatch(addCurrentUser(claims)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);

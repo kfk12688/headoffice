4f02e21a15c5be46583fe0a1c2a7ede090ddb6f8
sivask94 <sivask94@gmail.com>
2016-11-08 13:18:02 +0530

    Navigator styles changed

--- src/routes/_components/Navigator/index.js
+++ src/routes/_components/Navigator/index.js
@@ -2,55 +2,50 @@
  * Created by sharavan on 15/05/16.
  */
 import React from "react";
-import { TabGroup, Tab, NavLink, PopupButton } from "components";
-import styles from "./Navigator.less";
+import {TabGroup, Tab, NavLink, PopupButton} from "components";
 import Logo from "../../_styles/logo";
 
 const Navigator = (props) => {
-  const { user, logoutUser, className } = props;
-  const { username, name, site } = !!user && user;
+  const {user, logoutUser, className} = props;
+  const {username, name, site} = !!user && user;
 
   const popupStyle = {
-    color    : "inherit",
-    fontSize : "inherit",
-    padding  : "inherit",
+    color: "inherit",
+    fontSize: "inherit",
+    padding: "inherit",
   };
 
   return (
-    <div className={className}>
-      <div className={styles.brand}><Logo size="18" light/> Head Office App</div>
+    <nav className="navbar navbar-full navbar-dark bg-inverse">
+      <div className="row">
+        <div className="col-md-10 offset-md-1">
+          <a className="navbar-brand" href="#" style={{color : "red"}}><Logo size="20" light/> Head Office App</a>
 
-      <div className={styles.nav}>
-        <TabGroup childClassName={styles.link}>
-          <Tab><NavLink to="/template">Templates</NavLink></Tab>
-          <Tab><NavLink to="/data">Data</NavLink></Tab>
-          <Tab><NavLink to="/view">Reports</NavLink></Tab>
-          <Tab><NavLink to="/user">Users</NavLink></Tab>
-          <Tab><NavLink to="/workbooks">Workbooks</NavLink></Tab>
-        </TabGroup>
-      </div>
+          <div className="nav navbar-nav">
+            <NavLink className="nav-item nav-link" to="/template">Templates</NavLink>
+            <NavLink className="nav-item nav-link" to="/data">Data</NavLink>
+            <NavLink className="nav-item nav-link" to="/view">Reports</NavLink>
+            <NavLink className="nav-item nav-link" to="/user">Users</NavLink>
+            <NavLink className="nav-item nav-link" to="/workbooks">Workbooks</NavLink>
 
-      <div className={styles.assist}>
-        <TabGroup childClassName={styles.link}>
-          <Tab>Alerts</Tab>
-          <Tab>Help</Tab>
-          <Tab><PopupButton label={site} style={popupStyle}/></Tab>
-          <Tab>
-            <PopupButton label={name} style={popupStyle}>
-              <div>Logged in as {username}</div>
-              <div onClick={() => logoutUser()}>Sign Out</div>
-            </PopupButton>
-          </Tab>
-        </TabGroup>
+            <div className="pull-right">
+              <PopupButton label={site}/>
+              <PopupButton label={name}>
+                <div>Logged in as {username}</div>
+                <div onClick={() => logoutUser()}>Sign Out</div>
+              </PopupButton>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
 Navigator.propTypes = {
-  className  : React.PropTypes.string,
-  user       : React.PropTypes.object,
-  logoutUser : React.PropTypes.func,
+  className: React.PropTypes.string,
+  user: React.PropTypes.object,
+  logoutUser: React.PropTypes.func,
 };
 
-export { Navigator };
+export {Navigator};

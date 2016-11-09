3ffefc42cc922f0a3068a241e15dbb4a3e0f4946
sivask94 <sivask94@gmail.com>
2016-11-09 12:52:24 +0530

    integration of bootstrap styles in modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,29 +1,21 @@
-/**
- * Created by sharavan on 08/06/16.
- */
 import React from "react";
-import { Button } from "../Button/index";
+import {Button} from "../Button/index";
 import cx from "classnames";
 import BSModal from "react-overlays/lib/Modal";
 import styles from "./index.less";
 
-const Modal = ({ btnClassName, faName, caption, children, className, accent, show, toggleModal, modalTitle, title, bordered }) => {
+const Modal = ({btnClassName, faName, caption, children, className, accent, show, toggleModal, modalTitle, title, bordered}) => {
   const modalStyle = {
-    bottom   : 0,
-    left     : 0,
-    position : "fixed",
-    right    : 0,
-    top      : 0,
-    zIndex   : 1040,
-  };
-  const dialogStyle = {
-    left     : "28%",
-    position : "absolute",
-    top      : "15%",
+    bottom: 0,
+    left: 0,
+    position: "fixed",
+    right: 0,
+    top: 0,
+    zIndex: 1040,
   };
 
   return (
-    <div style={{ display : "inline-block" }}>
+    <div style={{display: "inline-block"}}>
       <Button
         className={btnClassName}
         faName={faName}
@@ -42,13 +34,15 @@ const Modal = ({ btnClassName, faName, caption, children, className, accent, sho
         show={show}
         onHide={toggleModal}
       >
-        <div style={dialogStyle} className={cx(styles.base, className)}>
-          <div>
-            <span className={styles.caption}>{modalTitle || caption}</span>
-            <i className={cx("fa fa-times", styles.closeButton)} onClick={toggleModal}/>
-          </div>
-          <div className={styles.content}>
-            {children}
+        <div className="modal-dialog modal-md">
+          <div className="modal-content">
+            <div className="modal-header">
+              <span className={styles.caption}>{modalTitle || caption}</span>
+              <i className={cx("fa fa-times", styles.closeButton)} onClick={toggleModal}/>
+            </div>
+            <div className="modal-body">
+              {children}
+            </div>
           </div>
         </div>
       </BSModal>
@@ -57,20 +51,20 @@ const Modal = ({ btnClassName, faName, caption, children, className, accent, sho
 };
 
 Modal.propTypes = {
-  accent       : React.PropTypes.oneOfType([
+  accent: React.PropTypes.oneOfType([
     React.PropTypes.string,
     React.PropTypes.bool,
   ]),
-  show         : React.PropTypes.bool.isRequired,
-  className    : React.PropTypes.string,
-  btnClassName : React.PropTypes.string,
-  faName       : React.PropTypes.string,
-  caption      : React.PropTypes.string,
-  toggleModal  : React.PropTypes.func.isRequired,
-  children     : React.PropTypes.node.isRequired,
-  modalTitle   : React.PropTypes.string,
-  title        : React.PropTypes.string,
-  bordered     : React.PropTypes.bool,
+  show: React.PropTypes.bool.isRequired,
+  className: React.PropTypes.string,
+  btnClassName: React.PropTypes.string,
+  faName: React.PropTypes.string,
+  caption: React.PropTypes.string,
+  toggleModal: React.PropTypes.func.isRequired,
+  children: React.PropTypes.node.isRequired,
+  modalTitle: React.PropTypes.string,
+  title: React.PropTypes.string,
+  bordered: React.PropTypes.bool,
 };
 
-export { Modal };
+export {Modal};

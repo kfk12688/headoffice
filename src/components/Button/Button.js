3bcb9243d1bc25184095a0ef02fa24735df574bd
jasminejanet <jasminegeorge93@gmail.com>
2016-11-09 12:08:17 +0530

    removes redundant props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,47 +1,7 @@
-/**
- * Created by sharavan on 12/05/16.
- */
 import React, { PropTypes } from "react";
-// import styles from "./Button.less";
 import cx from "classnames";
 
-// const Button = ({ className, faName, faClassName, after, accent, children, bordered, ...rest }) => {
-//   const faIcon = faName && <i className={cx(`fa fa-${faName}`, faClassName)}/>;
-
-//   let style = styles.button;
-//   if (bordered) style = cx(styles.button, styles.bordered);
-//   if (accent) style = styles.buttonBrdRed;
-//   if (accent === "green") style = styles.buttonBrdGreen;
-//   if (accent === "indigo") style = styles.buttonBrdIndigo;
-
-//   if (after) {
-//     return (
-//       <button
-//         className={cx(style, className)}
-//         type="button"
-//         {...rest}
-//       >
-//         {children}
-//         {" "}
-//         {faIcon}
-//       </button>
-//     );
-//   }
-
-//   return (
-//     <button
-//       className={cx(style, className)}
-//       type="button"
-//       {...rest}
-//     >
-//       {faIcon}
-//       {" "}
-//       {children}
-//     </button>
-//   );
-// };
-
-const Button = ({ disabled, size, style, outline, block, faName, faClassName, after, accent, children, bordered, ...rest }) => {
+const Button = ({ disabled, size, style, outline, block, faName, faClassName, children, ...rest }) => {
   if (style === undefined) style = "secondary";
   const outlineFlag = outline ? "-outline" : "";
   const faIcon = faName && <i className={cx(`fa fa-${faName}`, faClassName)}/>;
@@ -69,12 +29,6 @@ Button.propTypes = {
   faName      : PropTypes.string,
   faClassName : PropTypes.string,
   children    : PropTypes.any,
-  after       : PropTypes.bool,
-  accent      : PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.boolean,
-  ]),
-  bordered    : PropTypes.bool,
   style       : PropTypes.string,
   outline     : PropTypes.bool,
   block       : PropTypes.bool,

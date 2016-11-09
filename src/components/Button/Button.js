9227b659595dce1e806359b070d5550e2b06c96e
jasminejanet <jasminegeorge93@gmail.com>
2016-11-09 12:00:22 +0530

    integrates bootstrap styles in Button Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,41 +2,64 @@
  * Created by sharavan on 12/05/16.
  */
 import React, { PropTypes } from "react";
-import styles from "./Button.less";
+// import styles from "./Button.less";
 import cx from "classnames";
 
-const Button = ({ className, faName, faClassName, after, accent, children, bordered, ...rest }) => {
+// const Button = ({ className, faName, faClassName, after, accent, children, bordered, ...rest }) => {
+//   const faIcon = faName && <i className={cx(`fa fa-${faName}`, faClassName)}/>;
+
+//   let style = styles.button;
+//   if (bordered) style = cx(styles.button, styles.bordered);
+//   if (accent) style = styles.buttonBrdRed;
+//   if (accent === "green") style = styles.buttonBrdGreen;
+//   if (accent === "indigo") style = styles.buttonBrdIndigo;
+
+//   if (after) {
+//     return (
+//       <button
+//         className={cx(style, className)}
+//         type="button"
+//         {...rest}
+//       >
+//         {children}
+//         {" "}
+//         {faIcon}
+//       </button>
+//     );
+//   }
+
+//   return (
+//     <button
+//       className={cx(style, className)}
+//       type="button"
+//       {...rest}
+//     >
+//       {faIcon}
+//       {" "}
+//       {children}
+//     </button>
+//   );
+// };
+
+const Button = ({ disabled, size, style, outline, block, faName, faClassName, after, accent, children, bordered, ...rest }) => {
+  if (style === undefined) style = "secondary";
+  const outlineFlag = outline ? "-outline" : "";
   const faIcon = faName && <i className={cx(`fa fa-${faName}`, faClassName)}/>;
 
-  let style = styles.button;
-  if (bordered) style = cx(styles.button, styles.bordered);
-  if (accent) style = styles.buttonBrdRed;
-  if (accent === "green") style = styles.buttonBrdGreen;
-  if (accent === "indigo") style = styles.buttonBrdIndigo;
-
-  if (after) {
-    return (
-      <button
-        className={cx(style, className)}
-        type="button"
-        {...rest}
-      >
-        {children}
-        {" "}
-        {faIcon}
-      </button>
-    );
-  }
+  let className = cx("btn", `btn${outlineFlag}-${style}`, "btn-sm");
+  if (size === "lg") className = cx("btn", `btn${outlineFlag}-${style}`, "btn-lg");
+  if (size === "md") className = cx("btn", `btn${outlineFlag}-${style}`, "btn-md");
+  if (block) className = cx(className, "btn-block");
+  if (disabled) className = cx(className, "disabled");
 
   return (
     <button
-      className={cx(style, className)}
       type="button"
+      className={className}
+      disabled={disabled}
       {...rest}
     >
-      {faIcon}
-      {" "}
-      {children}
+      {children}&nbsp;{faIcon}
     </button>
   );
 };
@@ -52,6 +75,10 @@ Button.propTypes = {
     PropTypes.boolean,
   ]),
   bordered    : PropTypes.bool,
+  style       : PropTypes.string,
+  outline     : PropTypes.bool,
+  block       : PropTypes.bool,
+  disabled    : PropTypes.bool,
 };
 
 export { Button };
